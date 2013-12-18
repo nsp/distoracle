094f0535df880f19e5e0e8affcf653e99efab90b
Nate Parsons <parsons.nate@gmail.com>
2013-12-18 16:24:58 -0500

    Refactors sssp into own fn

--- src/sssp.cu
+++ src/sssp.cu
@@ -67,83 +67,31 @@ DijkstraKernel2(uint32  no_of_nodes,
         exit(1);                                                      \
     } }
 
-void copyToDevice(void *h_mem, void **d_mem, size_t size) {
+void copyToDevice(const void *h_mem, void **d_mem, size_t size) {
   CUDA_CHECK_RETURN( cudaMalloc( d_mem, size) );
   CUDA_CHECK_RETURN( cudaMemcpy( *d_mem, h_mem, size, cudaMemcpyHostToDevice) );
 }
 
-int32 main( int32 argc, char** argv) {
+void sssp( const uint32 no_of_nodes,
+	   const uint32 *h_graph_nodes,
+	   const uint32 no_of_edges,
+	   const uint32 *h_graph_edges,
+	   const uint32 *h_graph_weights,
+	   const uint32 source_id,
+	   uint32 *h_cost) {
 
   const uint32 MAX_COST = 1 << 30;
 
-  uint32 no_of_nodes = 0;
-  uint32 no_of_edges = 0;
-  
-  printf("Reading File\n");
-  // Read in Graph from a file
-  FILE *fp = fopen("NY.out","r");
-  if(!fp) {
-    printf("Error Reading graph file\n");
-    return -1;
+  uint32 *h_up_cost     = (uint32*) malloc(sizeof(uint32)*no_of_nodes);
+  bool   *h_graph_mask  = (bool*)   malloc(sizeof(bool)*no_of_nodes);
+  for( uint32 i=0; i<no_of_nodes; i++) {
+    h_up_cost[i]    = MAX_COST;
+    h_cost[i]       = MAX_COST;
+    h_graph_mask[i] = false;
   }
 
-  int32 source = 0;
-
-  fscanf(fp,"%d",&no_of_nodes);
-  printf("No of Nodes: %d\n",no_of_nodes);
-
-  // Make execution Parameters according to the number of nodes
-  // Distribute threads across multiple Blocks if necessary
-  uint32 num_of_blocks = 1;
-  uint32 num_of_threads_per_block = no_of_nodes;
-  if(no_of_nodes>MAX_THREADS_PER_BLOCK) {
-    num_of_blocks = (uint32)ceil(no_of_nodes/(double)MAX_THREADS_PER_BLOCK);
-    num_of_threads_per_block = MAX_THREADS_PER_BLOCK;
-  }
-
-  // allocate host memory
-  uint32* h_graph_nodes = (uint32*) malloc(sizeof(uint32)*no_of_nodes);
-  bool *h_graph_mask = (bool*) malloc(sizeof(bool)*no_of_nodes);
-  uint32 *h_up_cost = (uint32*) malloc(sizeof(uint32)*no_of_nodes);
-
-  uint32 start, edgeno;
-  // initalize the memory
-  for( uint32 i = 0; i < no_of_nodes; i++ ) {
-    fscanf(fp,"%d %d",&start,&edgeno);
-    h_graph_nodes[i] = start;
-    h_up_cost[i] = MAX_COST;
-    h_graph_mask[i]=false;
-  }
-
-  //read the source int from the file
-  fscanf(fp,"%d",&source);
-  printf("Source vid: %d\n", source);
-
-  //set the source int as true in the mask
-  h_graph_mask[source]=true;
-
-  fscanf(fp,"%d",&no_of_edges);
-  printf("No of Edges: %d\n", no_of_edges);
-
-  uint32 id;
-  uint32* h_graph_edges = (uint32*) malloc(sizeof(uint32)*no_of_edges);
-  uint32* h_graph_weights = (uint32*) malloc(sizeof(uint32)*no_of_edges);
-  for(uint32 i=0; i < no_of_edges ; i++) {
-    fscanf(fp,"%d",&id);
-    h_graph_edges[i] = id;
-    fscanf(fp,"%d",&id);
-    h_graph_weights[i] = id;
-  }
-
-  if(fp) fclose(fp);
-
-  printf("Read File\n");
-  printf("Avg Branching Factor: %f\n",no_of_edges/(float)no_of_nodes);
-
-  // allocate mem for the result on host side
-  uint32* h_cost = (uint32*) malloc( sizeof(uint32)*no_of_nodes);
-  for(uint32 i=0;i<no_of_nodes;i++) h_cost[i]= MAX_COST;
-  h_cost[source]=0;
+  h_cost[source_id]=0;
+  h_graph_mask[source_id] = true;
 
   uint32 *d_cost, *d_up_cost, *d_graph_nodes, *d_graph_edges, *d_graph_weights;
   bool *d_graph_mask;
@@ -161,6 +109,14 @@ int32 main( int32 argc, char** argv) {
   CUDA_CHECK_RETURN( cudaMalloc( (void**) &d_finished, sizeof(bool)));
 
   // setup execution parameters
+  // Make execution Parameters according to the number of nodes
+  // Distribute threads across multiple Blocks if necessary
+  uint32 num_of_blocks = 1;
+  uint32 num_of_threads_per_block = no_of_nodes;
+  if(no_of_nodes>MAX_THREADS_PER_BLOCK) {
+    num_of_blocks = (uint32)ceil(no_of_nodes/(double)MAX_THREADS_PER_BLOCK);
+    num_of_threads_per_block = MAX_THREADS_PER_BLOCK;
+  }
   dim3  grid( num_of_blocks, 1, 1);
   dim3  threads( num_of_threads_per_block, 1, 1);
 
@@ -190,6 +146,77 @@ int32 main( int32 argc, char** argv) {
   // copy result from device to host
   CUDA_CHECK_RETURN( cudaMemcpy( h_cost, d_cost, sizeof(int)*no_of_nodes, cudaMemcpyDeviceToHost) );
 
+  free(h_graph_mask);
+  free(h_up_cost);
+  CUDA_CHECK_RETURN(cudaFree(d_graph_nodes));
+  CUDA_CHECK_RETURN(cudaFree(d_graph_edges));
+  CUDA_CHECK_RETURN(cudaFree(d_graph_mask));
+  CUDA_CHECK_RETURN(cudaFree(d_graph_weights));
+  CUDA_CHECK_RETURN(cudaFree(d_up_cost));
+  CUDA_CHECK_RETURN(cudaFree(d_cost));
+  CUDA_CHECK_RETURN(cudaFree(d_finished));
+}
+
+int32 main( int32 argc, char** argv) {
+
+  uint32 no_of_nodes = 0;
+  uint32 no_of_edges = 0;
+  
+  printf("Reading File\n");
+  // Read in Graph from a file
+  FILE *fp = fopen("NY.out","r");
+  if(!fp) {
+    printf("Error Reading graph file\n");
+    return -1;
+  }
+
+  int32 source_id = 0;
+
+  fscanf(fp,"%d",&no_of_nodes);
+  printf("No of Nodes: %d\n",no_of_nodes);
+
+  // allocate host memory
+  uint32 *h_graph_nodes = (uint32*) malloc(sizeof(uint32)*no_of_nodes);
+
+  // initalize the memory
+  uint32 start, edgeno;
+  for( uint32 i = 0; i < no_of_nodes; i++ ) {
+    fscanf(fp,"%d %d",&start,&edgeno);
+    h_graph_nodes[i] = start;
+  }
+
+  //read the source int from the file
+  fscanf(fp,"%d",&source_id);
+  printf("Source vid: %d\n", source_id);
+
+  fscanf(fp,"%d",&no_of_edges);
+  printf("No of Edges: %d\n", no_of_edges);
+
+  uint32 id;
+  uint32* h_graph_edges = (uint32*) malloc(sizeof(uint32)*no_of_edges);
+  uint32* h_graph_weights = (uint32*) malloc(sizeof(uint32)*no_of_edges);
+  for(uint32 i=0; i < no_of_edges ; i++) {
+    fscanf(fp,"%d",&id);
+    h_graph_edges[i] = id;
+    fscanf(fp,"%d",&id);
+    h_graph_weights[i] = id;
+  }
+
+  if(fp) fclose(fp);
+
+  printf("Read File\n");
+  printf("Avg Branching Factor: %f\n",no_of_edges/(float)no_of_nodes);
+
+  // allocate mem for the result on host side
+  uint32* h_cost = (uint32*) malloc( sizeof(uint32)*no_of_nodes);
+
+  for(source_id=0; source_id<no_of_nodes; source_id++) {
+    sssp( no_of_nodes, h_graph_nodes,
+	  no_of_edges, h_graph_edges, h_graph_weights,
+	  source_id,
+	  h_cost );
+  }
+
   printf("Computation finished\n");
 
   //Store the result into a file
@@ -202,17 +229,8 @@ int32 main( int32 argc, char** argv) {
   // cleanup memory
   free(h_graph_nodes);
   free(h_graph_edges);
-  free(h_graph_mask);
   free(h_graph_weights);
-  free(h_up_cost);
   free(h_cost);
-  CUDA_CHECK_RETURN(cudaFree(d_graph_nodes));
-  CUDA_CHECK_RETURN(cudaFree(d_graph_edges));
-  CUDA_CHECK_RETURN(cudaFree(d_graph_mask));
-  CUDA_CHECK_RETURN(cudaFree(d_graph_weights));
-  CUDA_CHECK_RETURN(cudaFree(d_up_cost));
-  CUDA_CHECK_RETURN(cudaFree(d_cost));
-  CUDA_CHECK_RETURN(cudaFree(d_finished));
   CUDA_CHECK_RETURN(cudaDeviceReset());
   return 0;
 }
